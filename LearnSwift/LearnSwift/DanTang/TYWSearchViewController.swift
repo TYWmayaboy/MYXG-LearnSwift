017d01fcddea51fda448fec71b28886094103b62
tyw <18759280805@163.com>
2016-12-01 17:50:51 +0800

    Learning

--- LearnSwift/LearnSwift/DanTang/TYWSearchViewController.swift
+++ LearnSwift/LearnSwift/DanTang/TYWSearchViewController.swift
@@ -8,28 +8,49 @@
 
 import UIKit
 
-class TYWSearchViewController: UIViewController {
+class TYWSearchViewController: TYWBaseViewController {
 
+    var results = [TYWSearchResult]()
+    
+    weak var collectionView: UICollectionView?
+    
+    private lazy var searchBar: UISearchBar = {
+    
+        let searchBar = UISearchBar()
+        
+        searchBar.placeholder = "搜索商品、专题"
+        
+        return searchBar
+    }()
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        super.viewDidAppear(animated)
+        
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setupNav() {
+    
+        navigationItem.titleView = searchBar
+        
+        searchBar.delegate = self
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: UIView())
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "取消", style: .plain, target: self, action: #selector(navigationBackClick))
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func navigationBackClick() {
+        
+        navigationController?.popViewController(animated: true)
+    
     }
-    */
 
 }
